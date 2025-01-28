a2b426ba1ecf63720fa24a5fc21f288f84197b31
v.dzhgerenaya <v.dzhgerenaya@bft.ru>
2025-01-28 13:48:01 +0300

    ...

--- pambilight.py
+++ pambilight.py
@@ -3,17 +3,18 @@ import mss
 import numpy as np
 import serial
 
-bright_r = 0.15
-delta_r = 0  # 20
-bright_g = 0.1
-delta_g = 0  # 25
-bright_b = 0.1
-delta_b = 0  # 22
-led_by_horizontal = 21
-led_by_vertical = 12
+
+BRIGHT_R = 0.3
+DELTA_R = 0  # 20
+BRIGHT_G = 0.2
+DELTA_G = 0  # 25
+BRIGHT_B = 0.2
+DELTA_B = 0  # 22
+LED_BY_HARIZONTAL = 21
+LED_BY_VERTICAL = 12
 
 
-def get_leds_color(sct, srl, screen_width: int, screen_height: int, segment_width: int, segment_height: int, led_by_horizontal: int, led_by_vertical: int) -> list:
+def get_leds_color(sct, screen_width: int, screen_height: int, segment_width: int, segment_height: int, led_by_horizontal: int, led_by_vertical: int) -> list:
     # Result in the BGR, not RGB = [[B], [G], [R]]
     result =  []
     average_color = None
@@ -64,38 +65,43 @@ def get_leds_color(sct, srl, screen_width: int, screen_height: int, segment_widt
                 average_color = np.mean(image_array, axis=(0, 1))
         else:
             exit()
-        # result.append([round(average_color[0] * bright_b), round(average_color[1] * bright_g), round(average_color[2] * bright_r)])
+        # result.append([round(average_color[0] * BRIGHT_B), round(average_color[1] * BRIGHT_G), round(average_color[2] * BRIGHT_R)])
         result.append([round(
-            average_color[0] * bright_b - delta_b if average_color[0] * bright_b - delta_b > 0 else 0
+            average_color[0] * BRIGHT_B - DELTA_B if average_color[0] * BRIGHT_B - DELTA_B > 0 else 0
         ), round(
-            average_color[1] *  bright_g - delta_g if average_color[1] *  bright_g - delta_g > 0 else 0
+            average_color[1] * BRIGHT_G - DELTA_G if average_color[1] * BRIGHT_G - DELTA_G > 0 else 0
         ), round(
-            average_color[2] * bright_r - delta_r if average_color[2] * bright_r - delta_r > 0 else 0
+            average_color[2] * BRIGHT_R - DELTA_R if average_color[2] * BRIGHT_R - DELTA_R > 0 else 0
         )])
     return result
 
 
+def main():
 # Подключаемся к экрану
-with mss.mss() as sct:
-    # Определяем размер экрана...
-    screen_width = sct.monitors[1].get("width", 0)
-    screen_height = sct.monitors[1].get("height", 0)
-    # Подсчитываем размер сегмента изображения...
-    segment_width = screen_width // led_by_horizontal
-    segment_height = screen_height // led_by_vertical
-    # Подключаемся к Ардуино...
-    with serial.Serial('/dev/ttyUSB0', 115200) as srl:
-        while True:
-            # Делаем обработку 30 кадров в сек, для уменьшения нагрузки на процессор...
-            time.sleep(0.03333)
-            # Кодируем префикс обращения как Adalight, что бы работало и в Ambibox...
-            prefix = (ord('A'), ord('d'), ord('a'))
-            hi = 10
-            lo = 5
-            chk = 90
-            # Передаем шифр
-            srl.write(bytearray([*prefix, hi, lo, chk]))
-            # Получаем значение цвета для каждого светодиода...
-            leds =get_leds_color(sct, srl, screen_width, screen_height, segment_width, segment_height, led_by_horizontal, led_by_vertical)
-            for led in leds:
-                srl.write(bytearray([led[2] if led[2] > 0 else 0, led[1] if led[1] > 0 else 0, led[0] if led[0] > 0 else 0]))
+    with mss.mss() as sct:
+        # Определяем размер экрана...
+        screen_width = sct.monitors[1].get("width", 0)
+        screen_height = sct.monitors[1].get("height", 0)
+        # Подсчитываем размер сегмента изображения...
+        segment_width = screen_width // LED_BY_HARIZONTAL
+        segment_height = screen_height // LED_BY_VERTICAL
+        # Подключаемся к Ардуино...
+        with serial.Serial('/dev/ttyUSB0', 115200) as srl:
+            while True:
+                # Делаем обработку 30 кадров в сек, для уменьшения нагрузки на процессор...
+                time.sleep(0.03333)
+                # Кодируем префикс обращения как Adalight, что бы работало и в Ambibox...
+                prefix = (ord('A'), ord('d'), ord('a'))
+                hi = 10
+                lo = 5
+                chk = 90
+                # Передаем шифр
+                srl.write(bytearray([*prefix, hi, lo, chk]))
+                # Получаем значение цвета для каждого светодиода...
+                leds =get_leds_color(sct, screen_width, screen_height, segment_width, segment_height, LED_BY_HARIZONTAL, LED_BY_VERTICAL)
+                for led in leds:
+                    srl.write(bytearray([led[2] if led[2] > 0 else 0, led[1] if led[1] > 0 else 0, led[0] if led[0] > 0 else 0]))
+
+
+if __name__=="__main__":
+    main()
